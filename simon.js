7647d8fe747558d06cc067bf3bf6c5c716ab0b1f
robgaston1 <robcoach0221@gmail.com>
2017-01-07 06:26:32 -0500

    added functions playSequence and buttonPlay

--- simon.js
+++ simon.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
     
+    
+    function generateSequence(addToArray) {
+        var num = Math.floor(Math.random() * 4) + 1;
+        addToArray.push(num);
+        playSequence(addToArray);
+    }
+    
+    function buttonPlay(btnNum) {
+        console.log("Playing button!");
+    }
+    
+    function playSequence(sqnceArr) {
+        var arrLen = sqnceArr.length;
+        
+        for (var i = 0; i < arrLen; i++) {
+           
+            
+            setTimeout(buttonPlay(i), 1000);
+        }
+    }
+    
+    
 });
