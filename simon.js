03252d5f6ccd0adf06dca0604bba5ff369b33431
robgaston1 <robcoach0221@gmail.com>
2017-01-08 12:24:44 -0500

    Commented steps needed in buttonPlay function, added colors for lighted effect

--- simon.js
+++ simon.js
@@ -1,25 +1,40 @@
 $(document).ready(function () {
+    var sqnceArr = [];
+    var count = 0;
+    
+    $('.red').css('border-color', '#f5545d');
+    $('.blue').css('border-color', '#79b8fb');
+    $('.yellow').css('border-color', '#ffd82e');
     
     
-    function generateSequence(addToArray) {
+    function generateSequence() {
         var num = Math.floor(Math.random() * 4) + 1;
-        addToArray.push(num);
-        playSequence(addToArray);
+        sqnceArr.push(num);
+        playSequence();
+        count++;
+    }
+    
+    function playGreen(){
+        $('#sound').get(0).play();
+        $('.green').css('border-color','#6BF9A2');
+        
+        setTimeout(function(){$('.green').css('border-color','#00A74A');} , 1000);
     }
     
     function buttonPlay(btnNum) {
-        console.log("Playing button!");
+        //need to iterate through sqnceArr [1,4,2,3,4,1]
+        //playGreen; playBlue; playRed; playGreen; playGreen; playRed;
+        //for (var i = 0; i< sqnceArr.length; i++)
+        //the number in the array will help direct which button should play
+        //the button should change color and play the tone for one second
     }
     
-    function playSequence(sqnceArr) {
+    function playSequence() {
         var arrLen = sqnceArr.length;
-        
         for (var i = 0; i < arrLen; i++) {
-           
-            
             setTimeout(buttonPlay(i), 1000);
         }
     }
-    
+   playGreen(); 
     
 });
