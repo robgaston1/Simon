aeadfa147f321ad7c9ffbe6823c23affde73b5b0
robgaston1 <robcoach0221@gmail.com>
2017-01-08 21:54:03 -0500

    Fixed the function call with setTimeout. Tied number generator to sequence array and to light up buttons

--- simon.js
+++ simon.js
@@ -1,40 +1,63 @@
 $(document).ready(function () {
     var sqnceArr = [];
-    var count = 0;
-    
-    $('.red').css('border-color', '#f5545d');
-    $('.blue').css('border-color', '#79b8fb');
-    $('.yellow').css('border-color', '#ffd82e');
-    
-    
+    var sounds = document.getElementById("sound");
+    sounds.preload = "auto";
+     
     function generateSequence() {
         var num = Math.floor(Math.random() * 4) + 1;
         sqnceArr.push(num);
+        console.log(sqnceArr);
         playSequence();
-        count++;
     }
     
     function playGreen(){
-        $('#sound').get(0).play();
-        $('.green').css('border-color','#6BF9A2');
-        
-        setTimeout(function(){$('.green').css('border-color','#00A74A');} , 1000);
+        $('.green').css('border-color', '#6BF9A2');
+        setTimeout(function(){$('.green').css('border-color','#00A74A');} , 700);
+    }    
+    
+    function playRed(){
+        $('.red').css('border-color', '#f5545d');
+        setTimeout(function(){$('.red').css('border-color','#9F0F17');} , 700);
+    } 
+    
+    function playBlue(){
+        $('.blue').css('border-color', '#79b8fb');
+        setTimeout(function(){$('.blue').css('border-color','#094A8F');} , 700);
+    }
+    
+    function playYellow(){
+        $('.yellow').css('border-color', '#ffd82e');
+        setTimeout(function(){$('.yellow').css('border-color','#CCA707');} , 700);
     }
     
     function buttonPlay(btnNum) {
-        //need to iterate through sqnceArr [1,4,2,3,4,1]
-        //playGreen; playBlue; playRed; playGreen; playGreen; playRed;
-        //for (var i = 0; i< sqnceArr.length; i++)
-        //the number in the array will help direct which button should play
-        //the button should change color and play the tone for one second
+        switch(btnNum) {
+            case 1:
+                playGreen();
+                break;
+            case 2:
+                playRed();
+                break;
+            case 3:
+                playBlue();
+                break;
+            case 4:
+                playYellow();
+        }
     }
     
     function playSequence() {
         var arrLen = sqnceArr.length;
+        $('#count').text(arrLen);
         for (var i = 0; i < arrLen; i++) {
-            setTimeout(buttonPlay(i), 1000);
+            var arg = sqnceArr[i];
+            function callTimer(arg, scnds){
+                setTimeout(function() {buttonPlay(arg);}, i * 1000 );
+            } 
+            callTimer(arg, i);
         }
     }
-   playGreen(); 
+   
+    $('#generate').click(generateSequence);
     
 });
