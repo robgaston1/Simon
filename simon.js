ad8ee9122085ad6784fda3c799ad7df8b30d52a5
robgaston1 <robcoach0221@gmail.com>
2017-01-10 14:05:22 -0500

    Adding back and forth between computer generating buttons and player moves. Right now if wrong alert box pops up then game stops

--- simon.js
+++ simon.js
@@ -1,53 +1,67 @@
+
+
 $(document).ready(function () {
-    var sqnceArr = []; presses = -1; sounds = document.getElementById("sound");
-    sounds.preload = "auto";
+    var sqnceArr = [];
+    presses = 0;
 
     
     function playGreen() {
         $('.green').css('border-color', '#6BF9A2');
-        setTimeout(function(){$('.green').css('border-color','#00A74A');} , 700);
-    }    
+        setTimeout(function () {$('.green').css('border-color', '#00A74A'); }, 700);
+    }
     
     function playRed() {
         $('.red').css('border-color', '#f5545d');
-        setTimeout(function(){$('.red').css('border-color','#9F0F17');} , 700);
-    } 
+        setTimeout(function () {$('.red').css('border-color', '#9F0F17'); }, 700);
+    }
     
     function playBlue() {
         $('.blue').css('border-color', '#79b8fb');
-        setTimeout(function() {$('.blue').css('border-color','#094A8F');} , 700);
+        setTimeout(function () {$('.blue').css('border-color', '#094A8F'); }, 700);
     }
     
     function playYellow() {
         $('.yellow').css('border-color', '#ffd82e');
-        setTimeout(function() {$('.yellow').css('border-color','#CCA707');} , 700);
+        setTimeout(function () {$('.yellow').css('border-color', '#CCA707'); }, 700);
     }
 
-    $('.green').data('object', {function: playGreen, button: 1}) ;
-    $('.red').data('object', {function: playRed, button: 2}) ;
-    $('.yellow').data('object', {function: playYellow, button: 3}) ;
-    $('.blue').data('object', {function: playBlue, button: 4}) ;
+    $('.green').data('object', {func: playGreen, button: 1});
+    $('.red').data('object', {func: playRed, button: 2});
+    $('.yellow').data('object', {func: playYellow, button: 3});
+    $('.blue').data('object', {func: playBlue, button: 4});
+    
+    
     
     function buttonPlay(btnNum) {
-        switch(btnNum) {
-            case 1:
-                playGreen();
-                break;
-            case 2:
-                playRed();
-                break;
-            case 3:
-                playYellow();
-                break;
-            case 4:
-                playBlue();
+        switch (btnNum) {
+        case 1:
+            playGreen();
+            break;
+        case 2:
+            playRed();
+            break;
+        case 3:
+            playYellow();
+            break;
+        case 4:
+            playBlue();
+            break;
         }
     }
     
-    function startGame(){
-        if ($('input').is(':checked')){
-        setTimeout(generateSequence, 500);
+    function playSequence() {
+        var arrLen, arg;
+        arrLen = sqnceArr.length;
+        $('#count').text(arrLen);
+        console.log(sqnceArr);
+        for (var i = 0; i < arrLen; i++) { 
+            arg = sqnceArr[i];
+            function callTimer(num, scnds){
+                setTimeout(function() {buttonPlay(num);}, i * 1000 );
+            } 
+            callTimer(arg, i);
         }
+        playerInput(); 
     }
     
     function generateSequence() {
@@ -56,41 +70,43 @@ $(document).ready(function () {
         playSequence();
     }
     
-    function playSequence() {
-        var arrLen = sqnceArr.length;
-        $('#count').text(arrLen);
-        for (var i = 0; i < arrLen; i++) {
-            var arg = sqnceArr[i];
-            function callTimer(arg, scnds){
-                setTimeout(function() {buttonPlay(arg);}, i * 1000 );
-            } 
-            callTimer(arg, i);
+    function startGame() {
+        if ($('input').is(':checked')) {
+            setTimeout(generateSequence, 500);
         }
-        playerInput(); 
     }
     
     function wrongPlay() {
         $('.play').off();
         alert("Sorry wrong button");
-        playSequence();
+        //playSequence();
     }
    
+    function checkTurn() {
+        console.log("checkTurn, presses:", presses);
+        if (presses < sqnceArr.length) {
+            playerInput();
+        } else {
+            presses = 0;
+            setTimeout(generateSequence, 1500)
+        }
+    }
+    
     function playerInput() {
-        //Return to listen for next button press
-        //if button press not correct, then play incorrect tone and play the sequence again. 
-        presses = 0;
-        while (presses < sqnceArr.length) {
-            $(".play").on("click", function() {
-                clicked = $(this).data('object').button;
-                if (clicked == sqnceArr[presses]) {
-                    presses++;
-                    $(this).data('object').function();
-                } else { 
-                    wrongPlay();
-                }
-            })
-           }
-        generateSequence();
+        console.log("playInput, presses:", presses);
+        $(".play").on("click", function() {
+            clicked = $(this).data('object').button;
+            if (clicked == sqnceArr[presses]) {
+                presses++;
+                $(this).data('object').func();   
+                $(".play").off();
+                checkTurn();
+            } else {
+                wrongPlay();
+            }
+            
+        });
+
     }//end of playerInput
         
     $('#start').click(startGame);
